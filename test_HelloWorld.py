1ae9f638a83de1ac76f4481fbede8c9091364b36
Alexander Bondarchuk <alexander.bondarchuk@gmail.com>
2025-04-26 12:03:43 +0200

    Implement string adder function and update tests for null and single value inputs

--- test_HelloWorld.py
+++
@@ -1,5 +0,0 @@
-import HelloWorld
-
-
-def test_hello_world():
-    assert 'Hello World!' == HelloWorld.hello_world()
--- test_push.py
+++ test_push.py
@@ -1,5 +1,11 @@
+# TODO: + пустой input
+# TODO: 1 значение в input
+
 import string_adder
-def test_string_adder_returns_none_null_input():
-    assert string_adder.add() is None
+def test_string_adder_returns_zero_null_input():
+    assert string_adder.add() == 0
+
+def test_string_adder_returns_single_value_in_input():
+    assert string_adder.add("1") == 1
 
 
