5e8b3bf8825bdae7530d9501b7f3822b89dc915b
Artem Kobzev <96703372+daelin44@users.noreply.github.com>
2025-04-26 13:37:14 +0300

    Changed add function to accept 1 argument

--- string_adder.py
+++ string_adder.py
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-def add():
-    return 0
-=======
 def add(*args):
     if not args:
         return 0
     if len(args) == 1:
         return int(args[0])
->>>>>>> 3142291 (Implement string adder function and update tests for null and single value inputs)
